ff36b74709bbb730a701109bb95c788935fed367
Gavin Stark <gstark@gstark.com>
2012-04-17 18:58:35 -0400

    initial models

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TampaRb201204::Application.config.secret_token = '882f6057c08f4c2cd5c7267caee95a4239f56c9ddc2b970b826e0ef861f25b1ba50066774cac2535208424c350a3884190618529d353833d6e3cd38dd0d1eb32'
