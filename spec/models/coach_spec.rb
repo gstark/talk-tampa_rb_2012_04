fb44c0a52330a47c4ca512f3a2c739255d1e9c7d
Gavin Stark <gstark@gstark.com>
2012-04-17 20:16:03 -0400

    Slow coach spec

--- spec/models/coach_spec.rb
+++ spec/models/coach_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe Coach do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "#all_players returns players associated to teams and to coaching sessions" do
+    coach_1 = FactoryGirl.create(:coach)
+    coach_2 = FactoryGirl.create(:coach)
+
+    team_1  = FactoryGirl.create(:team, :coach => coach_1)
+    team_2  = FactoryGirl.create(:team, :coach => coach_2)
+
+    player_1 = FactoryGirl.create(:player, :team => team_1)
+    player_2 = FactoryGirl.create(:player, :team => team_2)
+
+    FactoryGirl.create(:coaching_session, :student => player_1, :coach => coach_1)
+    FactoryGirl.create(:coaching_session, :student => team_2,   :coach => coach_1)
+
+    coach_1.all_players.should =~ [player_1, player_2]
+  end
 end
